565b751da0521388828d4298a5620c9be222d121
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2023-08-04 16:10:26 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('Hello world! My fisrt test on Python')
+print('Hello world! My fisrt test on Python!')
+
+#This is a commentary.
+
+name = "Nicolas"
+age = 18
+
+if age < 18 :
+    print("You are not of legal age!")
+else:
+    print("You are of legal age!")
+
+#The "print" command write something in the console.
