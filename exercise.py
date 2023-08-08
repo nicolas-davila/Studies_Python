36d15e6fbc6520b903e4d9a462778dfa800a1a19
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2023-08-08 17:28:57 -0300

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,3 +1,4 @@
+#Exercise 1
 name = input("Hello there! Whats your name? ")
 print("Hello ", name, "! My name is Python and I am a programming language! I will help you to do some math!")
 
@@ -7,3 +8,5 @@ y = int(input("Enter another number: "))
 result = x + y
 
 print("The sum of ", x, " and ", y, " is: ", result)
+
+#Exercise 2
