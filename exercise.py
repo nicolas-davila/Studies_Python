30aa1ec2dc140213c3558ba3f7c199336c7969bb
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2023-08-08 15:34:11 -0300

    commit

---
+++ exercise.py
@@ -0,0 +1,9 @@
+name = input("Hello there! Whats your name? ")
+print("Hello ", name, "! My name is Python and I am a programming language! I will help you to do some math!")
+
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+
+result = x + y
+
+print("The sum of ", x, " and ", y, " is: ", result)
--- test.py
+++ test.py
@@ -11,3 +11,9 @@ else:
     print("You are of legal age!")
 
 #The "print" command write something in the console.
+
+name = input("What is your name? ") #The "input" command is used to ask the user for data.
+age = int(input("How old are you? ")) #The "int" command is used to convert a string to an integer.
+print("Hello ", name, "!", " You are ", age, " years old!") #The "," is used to separate the data that is going to be printed.
+
+# print("Hello " + name, "!" + " You are " + str(age) + " years old!") --> #The "+" is used to concatenate strings.
